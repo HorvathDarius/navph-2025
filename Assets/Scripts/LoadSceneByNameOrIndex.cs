29f3783e4db5a8080ff5c0af13c71bf3094d6cb4
RikoAppDev <riko.appdev@gmail.com>
2025-11-06 15:29:14 +0100

    setup: scene swith loader and input system; feat(yeme): basic movement

--- Assets/Scripts/LoadSceneByNameOrIndex.cs
+++ Assets/Scripts/LoadSceneByNameOrIndex.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadSceneByIndex : MonoBehaviour
+public class LoadSceneByNameOrIndex : MonoBehaviour
 {
-    // General method to load scenes based on build index
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
     public void LoadScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
