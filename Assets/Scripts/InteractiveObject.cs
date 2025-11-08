9533489e682301bf9a2fc4b0f2961a6d8a032b94
Horvath Darius <dariushorvath33@gmail.com>
2025-11-08 11:52:17 +0100

    Create Item scriptable object + finish item pickup feature, add timer

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -8,35 +8,40 @@ public class InteractiveObject : MonoBehaviour
 
     private SpriteRenderer pickupArrowSpriteRenderer;
     private PlayerController playerController;
+    public Item itemData;
 
+    // Initialize pickup arrow
     void Start()
     {
         pickupArrowSpriteRenderer = pickupArrow.GetComponent<SpriteRenderer>();
         pickupArrowSpriteRenderer.enabled = false;
     }
 
+    // When player close, display arrow and set nearby object
     private void OnTriggerEnter2D(Collider2D other)
     {
         playerController = other.GetComponent<PlayerController>();
         if (playerController != null)
         {
-            Debug.Log("Player entered pickup range of: " + gameObject.name);
+            Debug.Log("Player entered pickup range of: " + itemData.itemName);
             pickupArrowSpriteRenderer.enabled = true;
             playerController.SetNearbyObject(this);
         }
     }
 
+    // When player leaves, hide arrow and clear nearby object
     private void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log("Player exited pickup range of: " + gameObject.name);
+        Debug.Log("Player exited pickup range of: " + itemData.itemName);
         pickupArrowSpriteRenderer.enabled = false;
     }
 
+    // Handle item pickup
     public void pickUpItem(PlayerController playerController)
     {
         Debug.Log("Calling AddItemToInventory");
-        playerController.AddItemToInvetory(gameObject);
-        Debug.Log("Destroying GameObject: " + gameObject.name);
+        playerController.AddItemToInvetory(itemData);
+        Debug.Log("Destroying GameObject: " + itemData.itemName);
         Destroy(gameObject);
     }
 }
